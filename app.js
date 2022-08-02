caef5a73a20f07fd9289ce8d3c93806db695da77
MaxiOrellanadev <maxiorellana.bru@gmail.com>
2022-08-02 01:04:10 -0300

    registro hecho

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const port = 3030;
 app.use(express.static("public"));
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views" , "home.html")))
+app.get("/register", (req,res) => res.sendFile(path.join(__dirname, "views" , "register.html")))
 
 app.listen(port , () => console.log("server running in http://localhost:" + port ))
